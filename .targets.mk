f94b72f180accda91b6d07a93eb7526c643fdb8a
Dan Garcia-Carrillo <garciadan@uniovi.es>
2025-02-17 19:30:34 +0100

    added lib folder

--- .targets.mk
+++
@@ -1,4 +0,0 @@
-TARGETS_DRAFTS := draft-ingles-eap-edhoc 
-TARGETS_TAGS := 
-draft-ingles-eap-edhoc-00.md: draft-ingles-eap-edhoc.md
-	sed -e 's/draft-ingles-eap-edhoc-latest/draft-ingles-eap-edhoc-00/g' $< >$@
